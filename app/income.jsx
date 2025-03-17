ba1622100139f2cfcd27ad3f5d5457c002840172
ranjitkc7 <ranjit252@gmail.com>
2025-03-17 21:59:38 +0545

    first-commit

---
+++ app/income.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const IncomePage = () => {
+  return (
+    <View>
+      <Text>IncomePage</Text>
+    </View>
+  )
+}
+
+export default IncomePage
